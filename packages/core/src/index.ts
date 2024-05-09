205c0af0585838a0975cf6ede6565e918c989ffd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-05-09 21:50:58 +0800

    fix(core/data): export all

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
 export type { Author } from './types/author.ts'
 export type { Comment } from './types/comment.ts'
 export type { CustomEmoji } from './types/custom-emoji.ts'
-export type { Data } from './types/data.ts'
+export type { Data, DataVendors } from './types/data.ts'
 export type { Reactions } from './types/reactions.ts'
 
 export { type Preset, type PresetTask, definePreset, definePresetTask } from './utils/preset.ts'
