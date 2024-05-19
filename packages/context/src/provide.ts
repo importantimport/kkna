13cae7f1de1ec126fb1507c1d709961b973b356b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-05-19 21:21:53 +0800

    chore(context): update comment

--- packages/context/src/provide.ts
+++ packages/context/src/provide.ts
@@ -6,6 +6,23 @@ export interface ProviderOptions {
   initialValue?: ContextType<typeof context>
 }
 
+/**
+ * Context Provider Wrapper.
+ * @param host HTMLElement
+ * @param options ProviderOptions
+ * @returns `new ContextProvider()`
+ * @example
+ * ```ts
+ * import { provider } from '@kkna/context'
+ * import { hatsu } from '@kkna/preset-hatsu'
+ *
+ * const provide = provider(document.body, {
+ *   initialValue: {
+ *     presets: [hatsu({ instance: 'https://hatsu.local' })]
+ *   }
+ * })
+ * ```
+ */
 export const provider = (host: HTMLElement, options?: ProviderOptions) => new ContextProvider(host, {
   ...options,
   context,
